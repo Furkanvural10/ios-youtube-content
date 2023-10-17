0e7aa1e74dfa6182a493212b1b4ad5b8990bfb56
Furkan Vural <furkanvural52@gmail.com>
2023-10-17 09:49:14 +0300

    Add Spacer folder

---
+++ Spacer/Spacer/SpacerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpacerApp.swift
+//  Spacer
+//
+//  Created by furkan vural on 17.10.2023.
+//
+
+import SwiftUI
+
+@main
+struct SpacerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
