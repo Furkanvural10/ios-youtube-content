1b946da25b470feeee6d6929286a26473dcc7877
Furkan Vural <furkanvural52@gmail.com>
2023-10-18 13:24:53 +0300

    Add TextView project

---
+++ TextView/TextView/TextViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  TextViewApp.swift
+//  TextView
+//
+//  Created by furkan vural on 18.10.2023.
+//
+
+import SwiftUI
+
+@main
+struct TextViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
