26140b2cea59cddeef513c3fb983494360b88ced
Furkan Vural <furkanvural52@gmail.com>
2023-10-21 20:06:26 +0300

    Add color with ignoresSafeArea code pieces

---
+++ Color/Color/ColorApp.swift
@@ -0,0 +1,17 @@
+//
+//  ColorApp.swift
+//  Color
+//
+//  Created by furkan vural on 20.10.2023.
+//
+
+import SwiftUI
+
+@main
+struct ColorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Color/Color/ContentView.swift
@@ -0,0 +1,32 @@
+//
+//  ContentView.swift
+//  Color
+//
+//  Created by furkan vural on 20.10.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        
+        Color.gray
+        
+//        MARK: - Color with ignoresSafeArea
+//        Color.gray
+//            .ignoresSafeArea()
+        
+        
+            
+            
+            
+            
+            
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
