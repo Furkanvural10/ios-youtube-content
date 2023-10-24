32513a72682b009c1d953057810bb446bed74a33
Furkan Vural <furkanvural52@gmail.com>
2023-10-24 13:39:28 +0300

    Add RoundedRectange example

--- Rectangle&Capsule&Elipse&Circle/Rectangle&Capsule&Elipse&Circle/ContentView.swift
+++ Rectangle&Capsule&Elipse&Circle/Rectangle&Capsule&Elipse&Circle/ContentView.swift
@@ -5,10 +5,21 @@ struct ContentView: View {
     var body: some View {
         
         // MARK: - Rectangle
-        Rectangle()
-            .fill(.red)
-            .frame(width: 200, height: 200)
-            .clipShape(RoundedRectangle(cornerRadius: 20))
+//        Rectangle()
+//            .fill(.red)
+//            .frame(width: 200, height: 200)
+//            .clipShape(RoundedRectangle(cornerRadius: 20))
+        
+                VStack(spacing: 50) {
+                    RoundedRectangle(cornerRadius: 20, style: .circular)
+                        .fill(.blue)
+                        .frame(width: 300, height: 70)
+        
+                    RoundedRectangle(cornerRadius: 20, style: .continuous)
+                        .fill(.orange)
+                        .frame(width: 300, height: 70)
+                }
+
         
 
         
