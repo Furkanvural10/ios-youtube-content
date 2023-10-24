39a740563be6b2a5f8a7604d40a63a5ecac52404
Furkan Vural <furkanvural52@gmail.com>
2023-10-24 13:42:20 +0300

    Add Circle example

--- Rectangle&Capsule&Elipse&Circle/Rectangle&Capsule&Elipse&Circle/ContentView.swift
+++ Rectangle&Capsule&Elipse&Circle/Rectangle&Capsule&Elipse&Circle/ContentView.swift
@@ -5,36 +5,41 @@ struct ContentView: View {
     var body: some View {
         
         // MARK: - Rectangle
-//        Rectangle()
-//            .fill(.red)
-//            .frame(width: 200, height: 200)
-//            .clipShape(RoundedRectangle(cornerRadius: 20))
-  
+        //        Rectangle()
+        //            .fill(.red)
+        //            .frame(width: 200, height: 200)
+        //            .clipShape(RoundedRectangle(cornerRadius: 20))
+        
         // MARK: - Rounded Rectangle
-//                VStack(spacing: 50) {
-//                    RoundedRectangle(cornerRadius: 20, style: .circular)
-//                        .fill(.blue)
-//                        .frame(width: 300, height: 70)
-//
-//                    RoundedRectangle(cornerRadius: 20, style: .continuous)
-//                        .fill(.orange)
-//                        .frame(width: 300, height: 70)
-//                }
-
+        //                VStack(spacing: 50) {
+        //                    RoundedRectangle(cornerRadius: 20, style: .circular)
+        //                        .fill(.blue)
+        //                        .frame(width: 300, height: 70)
+        //
+        //                    RoundedRectangle(cornerRadius: 20, style: .continuous)
+        //                        .fill(.orange)
+        //                        .frame(width: 300, height: 70)
+        //                }
+        
         // MARK: - Capsule
-//        Capsule()
-//            .fill(.pink)
-//            .frame(width: 300, height: 50)
+        //        Capsule()
+        //            .fill(.pink)
+        //            .frame(width: 300, height: 50)
+        
+        // MARK: - Elipse
+        //        Ellipse()
+        //            .fill(.blue)
+        //            .frame(width: 200, height: 100)
+        
+        // MARK: - Circle
+        Circle()
+            .fill(.cyan)
+            .frame(height: 200)
         
         
-        Ellipse()
-            .fill(.blue)
-            .frame(width: 200, height: 100)
         
-
-
         
-
+        
         
         
         
