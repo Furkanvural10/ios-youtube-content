a94b0c2da6fd5b0ed3eb6e4a30085ebc77fe11e7
Furkan Vural <furkanvural52@gmail.com>
2023-10-30 17:31:09 +0300

    Add basic json definition and example

--- Networking/MyPlayground.playground/Contents.swift
+++ Networking/MyPlayground.playground/Contents.swift
@@ -1,3 +1,30 @@
 import UIKit
+import Foundation
 
-var greeting = "Hello, playground"
+// MARK: - JSON
+
+let jsonExample = """
+
+{
+    "name": "Furkan",
+    "surname" : "Vural",
+    "age" : 29,
+    "isAvailable": true
+}
+
+""".data(using: .utf8)!
+
+do {
+    
+    let resultX = try JSONSerialization.jsonObject(with: jsonExample, options: .mutableContainers) as! [String: Any]
+
+    if let name = resultX["name"],
+       let surname = resultX["surname"] {
+        print(name)
+        print(surname)
+    }
+} catch {
+    print(error.localizedDescription)
+}
+
+
